b309864e4ce13fc3afef9cc23f9dbe01b960cce0
patrick.123.foster@gmail.com <patrick.123.foster@gmail.com>
2023-03-20 19:03:04 -0500

    haus analytics

--- src/components/Haus.js
+++ src/components/Haus.js
@@ -2,8 +2,15 @@ import { useEffect } from "react";
 
 const Haus = () => {
     useEffect(() => {
-        window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSd-bKO-rbVyyBtGjMCPPpilwpJevLJRi_J-bnYE2CnUtOFv9w/viewform";
+        fetchHaus();
     }, []);
+
+    //fetch the following with a get request https://wordsmith-api-production.up.railway.app/magic-the-gathering
+    const fetchHaus = async () => {
+        console.log('herea!');
+        const response = await fetch("https://wordsmith-api-production.up.railway.app/magic-the-gathering");
+        window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSd-bKO-rbVyyBtGjMCPPpilwpJevLJRi_J-bnYE2CnUtOFv9w/viewform";
+    }
 
     return (
         <h1></h1>

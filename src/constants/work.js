7ebcac3adb444983a3143f3d1163b76c3c0db399
paaatrrrick <patrick@mintlify.com>
2026-03-15 12:51:01 -0700

    No goose

--- src/constants/work.js
+++ src/constants/work.js
@@ -1,9 +1,5 @@
 const workExperience = [
     {
-        title: 'Goose farmer',
-        timeLine: 'May - August 2027. Seattle, WA',
-    },
-    {
         title: 'Founder at a stealth startup',
         timeLine: 'May - August 2026. Seattle, WA',
     },

ac4a8c09c65bba3d12138b370de1eabfe4cf8d52
paaatrrrick <patrick@mintlify.com>
2026-03-15 12:50:44 -0700

    no farmer

--- src/constants/work.js
+++ src/constants/work.js
@@ -1,9 +1,5 @@
 const workExperience = [
     {
-        title: 'Father',
-        timeLine: '2028. Seattle, WA',
-    },
-    {
         title: 'Goose farmer',
         timeLine: 'May - August 2027. Seattle, WA',
     },

99722c5c7e3d72a49394b38f09864351bdc8fd2c
paaatrrrick <patrick@mintlify.com>
2026-06-18 18:23:27 -0700

    Add recent commands to the command palette

--- src/commandPalette/index.js
+++ src/commandPalette/index.js
@@ -1,4 +1,5 @@
 export { default as CommandPalette } from "./CommandPalette";
 export { useCommandPalette } from "./useCommandPalette";
+export { useRecentCommands } from "./useRecentCommands";
 export { rankCommands, fuzzyScore } from "./fuzzy";
 export { commands, commandsByGroup, COMMAND_GROUPS } from "../constants/commands";
